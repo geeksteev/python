577f422d0f2cc9593102c1f433d871bdbe316339
Steve Heffernan <heffernanfamily02@gmail.com>
2021-05-05 14:20:12 -0400

    added file

---
+++ something.py
@@ -0,0 +1,26 @@
+import os
+
+get_actions()
+# get all actions from your director.
+
+parse_actions()
+# parse the actions to trim the amount of data you're working with. 
+# For example, pull in only actions that have the "A" (i forget that it means) in them.
+# Do not change any data here, only data collection.
+
+get_malware_names()
+# get malware information and return only malware names.
+
+#for malware_name in malware_names:
+#    do something
+
+#return(malware_name)
+
+get_malware_tags()
+# get malware information and return only malware tags.
+
+#for malware_tag in malware_tags:
+#    do something
+
+#return(malware_tag)
+
