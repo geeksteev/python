e76e5f37c29a9b4c097df032655a90fa6462e0aa
Steve Heffernan <heffernanfamily02@gmail.com>
2021-05-05 14:22:36 -0400

    slight change

--- something.py
+++ something.py
@@ -3,10 +3,14 @@ import os
 get_actions()
 # get all actions from your director.
 
+#return()
+
 parse_actions()
 # parse the actions to trim the amount of data you're working with. 
 # For example, pull in only actions that have the "A" (i forget that it means) in them.
 # Do not change any data here, only data collection.
+
+#return(data)
 
 get_malware_names()
 # get malware information and return only malware names.
